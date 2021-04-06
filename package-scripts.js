8fcf2c14c801f8fa8db65e14baf3e4b6f879ca72
Ramses Salas <ramjsm@github.com>
2021-04-06 17:34:30 +0200

    R3F presentation

--- package-scripts.js
+++ package-scripts.js
@@ -2,7 +2,8 @@ module.exports = {
   scripts: {
     present: {
       react: 'nps "start mdx/react.mdx"',
-      ea: 'nps "start mdx/ea.mdx"'
+      ea: 'nps "start mdx/ea.mdx"',
+      reactwebgl: 'nps "start mdx/reactWebGL.mdx"'
     },
     start: 'mdx-deck',
     build: 'mdx-deck build',
